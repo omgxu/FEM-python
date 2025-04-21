dabda7259e5325f1526a94906ef8022bcd86d17f
luanyu <1340517741@qq.com>
2025-04-21 21:27:22 +0800

    begin to do T4-12

--- elasticity2d-python/Exact.py
+++ elasticity2d-python/Exact.py
@@ -14,6 +14,7 @@ import numpy as np
 import math 
 import matplotlib.pyplot as plt
 import FEData as model
+from utitls import gauss
 
 from Elast2DElem import NmatElast2D, BmatElast2D
 
@@ -43,6 +44,9 @@ def Exact(ax1):
 
 	xplot = np.arange(a, b, dx)
 	line5, = ax1.plot(xplot, sigma_rr, 'r', label='Exact')
+
+def Exact_4_12():
+	pass
 
 def sigmarr():
 	"""
@@ -89,3 +93,53 @@ def sigmarr():
 		sigma_osp[index] = sigma_rr_e[10]
 		
 	return xplot, sigma_rr, xplot_osp, sigma_osp
+
+def sigmaxx_4_12():
+	pass
+
+def ErrorNorm_4_12():
+	"""
+	Calculate and print the error norm (L2 and energy norm) of displacement in T4-12
+	"""
+
+	w, gp = gauss(model.ngp)
+
+	L2Norm = 0.0
+	EnNorm = 0.0
+
+	for e in range(model.nel):
+
+		# get coordinate and displacement of element nodes
+		je = model.IEN[:, e] - 1
+		C = np.array([model.x[je], model.y[je]]).T
+		de = model.d[model.LM[:,e]-1]
+
+		for i in range(model.ngp):
+			for j in range(model.ngp):
+				eta = gp[i]
+				psi = gp[j]
+
+				xt = 0.5*(C[0,0]+C[1,0]+C[2,0]+C[3,0]) + 0.5*(C[0,1]+C[1,1]+C[2,1]+C[3,1])
+				yt = 0.5*(C[0,1]+C[1,1]+C[2,1]+C[3,1]) + 0.5*(C[0,0]+C[1,0]+C[2,0]+C[3,0])
+
+				N, detJ = NmatElast2D(eta, psi, C)
+				B, detJ = BmatElast2D(eta, psi, C)
+
+				u_h = N@de
+				u_ex = 0.5*xt + 0.5*yt  # Exact displacement for the problem T4-12
+				L2Norm += (u_h - u_ex)**2 * detJ * w[i] * w[j]
+
+				s_h = B@de
+				s_ex = 1.0  # Exact strain for the problem T4-12
+				EnNorm += (s_h - s_ex)**2 * detJ * w[i] * w[j]
+		
+		L2Norm = math.sqrt(L2Norm)
+		EnNorm = math.sqrt(EnNorm)
+
+		length = 5.0
+
+		N_h = math.sqrt(model.nel/5)
+
+		print("h = %13.6E, L2Norm = %13.6E, EnNorm = %13.6E" %(length/(5*N_h), L2Norm, EnNorm))
+
+		return L2Norm, EnNorm
